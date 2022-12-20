378df937347b995b1c0bbd6c9b4d112a57fc87cd
Agustin Saladino <agussala2003@gmail.com>
2022-12-20 20:43:23 -0300

    Proyecto por subir informacion

---
+++ js/index.js
@@ -0,0 +1,7 @@
+if (window.innerWidth < 1024){
+    document.querySelector('.links-container ul').innerHTML = `<li><a href="index.html"><i class='bx bx-home-alt'></i></a></li>
+    <li><a href="#about"><i class='bx bx-user'></i></a></li>
+    <li><a href="#skills"><i class='bx bx-medal' ></i></a></li>
+    <li><a href="#portfolio"><i class='bx bx-folder-open' ></i></a></li>
+    <li><a href="#contact"><i class='bx bx-envelope' ></i></a></li>`
+}
